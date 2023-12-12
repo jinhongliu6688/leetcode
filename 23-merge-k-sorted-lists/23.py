5a7509c7ca02e07d5c82f062e5bca55d0e28fa7b
Jinhong Liu <112588153+jinhongliu6688@users.noreply.github.com>
2023-12-11 20:28:51 -0800

    Update 23.py

--- 23-merge-k-sorted-lists/23.py
+++ 23-merge-k-sorted-lists/23.py
@@ -21,3 +21,35 @@ class Solution:
             node = node.next
         
         return start.next
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
+        node = ListNode()
+        start = node
+        not_none = len([x for x in lists if x])
+
+        while not_none > 0:
+            vals = []
+            for head in lists:
+                if head:
+                    vals.append(head.val)
+
+            min_val = min(vals)
+            vals.clear()
+
+            for i, head in enumerate(lists):
+                if head and head.val == min_val:
+                    node.next = head
+                    node = node.next
+                    head = head.next
+                    lists[i] = head
+                    if not head:
+                        not_none -= 1
+
+        return start.next
+                
