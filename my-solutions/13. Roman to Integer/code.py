adde182e2bf52b8ecf72d718561a4779495b8dd8
Jinhong Liu <112588153+jinhongliu6688@users.noreply.github.com>
2024-03-06 01:25:00 -0800

    Update code.py

--- my-solutions/13. Roman to Integer/code.py
+++ my-solutions/13. Roman to Integer/code.py
@@ -1,3 +1,4 @@
+# method 1
 class Solution:
     def romanToInt(self, s: str) -> int:
         num = 0
@@ -33,5 +34,28 @@ class Solution:
 
         for l in s:
             num += sym_val[l]
+
+        return num
+
+# method 2
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        num = 0
+
+        sym_val = {
+            "I" : 1,
+            "V" : 5,
+            "X" : 10,
+            "L" : 50,
+            "C" : 100,
+            "D" : 500,
+            "M" : 1000,
+        }
+
+        for i, letter in enumerate(s):
+            if i < len(s) - 1 and sym_val[s[i]] < sym_val[s[i + 1]] and i < len(s) - 1:
+                    num -= sym_val[letter]
+            else:
+                num += sym_val[letter]
 
         return num
