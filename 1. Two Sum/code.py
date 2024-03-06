04c4aacc14662a9860a121a814ad6026b76c0274
Jinhong Liu <112588153+jinhongliu6688@users.noreply.github.com>
2024-03-05 23:32:56 -0800

    Update code.py

--- 1. Two Sum/code.py
+++ 1. Two Sum/code.py
@@ -3,12 +3,17 @@
 
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
+        # Create an empty dictionary to store the numbers and their indices
         dict_1 = {}
 
+        # Iterate through the list of numbers along with their indices
         for i, num in enumerate(nums):
+            # if the dictionary is empty, add the current number to the dictionary as key and set its index as value
             if not dict_1:
                 dict_1[num] = i
+            # if the component of current number is in the dictionary, return the index of the current number and the index of the component
             elif target - num in dict_1:
                 return [i, dict_1[target - num]]
+            # if the current number does not exist in the dictionary, add the current number to the dictionary along with its index
             else:
                 dict_1[num] = i
