998f0d7c2eea07fb2a9c9491a359bf16917ac8fc
Jinhong Liu <112588153+jinhongliu6688@users.noreply.github.com>
2024-03-09 08:31:54 -0800

    Create code.py

---
+++ my-solutions/20. Valid Parentheses/code.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def isValid(self, s):
+        while "()" in s or "{}" in s or '[]' in s:
+            s = s.replace("()", "").replace('{}', "").replace('[]', "")
+        return s == ''
+
+
+# time: O(n^2)
+# space: O(n)
+class Solution:
+    def isValid(self, s: str) -> bool:
+        previous_length = len(s)
+        while len(s) > 0:
+            if "()" in s:
+                s = s.replace("()", "")
+            if "[]" in s:
+                s = s.replace("[]", "")
+            if "{}" in s:
+                s = s.replace("{}", "")
+            if len(s) == previous_length:
+                return False
+            previous_length = len(s)
+        return True
