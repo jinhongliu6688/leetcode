05eb2236ab38fed2ee96f3bdd41ea72f5b93eb43
Jinhong Liu <112588153+jinhongliu6688@users.noreply.github.com>
2024-03-09 09:00:04 -0800

    Update code.py

--- my-solutions/20. Valid Parentheses/code.py
+++ my-solutions/20. Valid Parentheses/code.py
@@ -26,16 +26,30 @@ class Solution:
 # space: O(n)
 class Solution:
     def isValid(self, s: str) -> bool:
+        # initilize an empty deque to serve as a stack
         stack = collections.deque()
+        
+        # iterate the letters in string s
         for l in s:
+            # if the stack is empty, append the letter to the stack 
             if not stack:
                 stack.append(l)
+            # if the current letter is (, check if the top element in the stack is ). If it is, pop the top element
             elif l == ")" and stack[-1] == "(":
                 stack.pop()
+            # if the current letter is [, check if the top element in the stack is ]. If it is, pop the top element
             elif l == "]" and stack[-1] == "[":
                 stack.pop()
+            # if the current letter is {, check if the top element in the stack is }. If it is, pop the top element
             elif l == "}" and stack[-1] == "{":
                 stack.pop()
+            # if the above conditions do not met, add the letter to the stack
+            # in other words, we add ( or [ or { in this step
+            else:
+                stack.append(l)
+        
+        # after the iteration, check if the stack is empty and return the result
+        return len(stack) == 0
             else:
                 stack.append(l)
         return len(stack) == 0
