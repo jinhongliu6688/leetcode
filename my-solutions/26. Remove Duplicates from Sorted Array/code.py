934749a66e5851d6064b326d968f39d987a686dd
Jinhong Liu <112588153+jinhongliu6688@users.noreply.github.com>
2024-03-12 20:05:55 -0700

    Create code.py

---
+++ my-solutions/26. Remove Duplicates from Sorted Array/code.py
@@ -0,0 +1,16 @@
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        # initialize insert_index to 1
+        insert_index = 1
+
+        # iterate the number is nums and skip the first number
+        for i in range(1, len(nums)):
+            # if the current number is different to the last number, 
+            # replace the number at the insertion index with the current number
+            if nums[i] != nums[i-1]:
+                nums[insert_index] = nums[i]
+                # move the insertion index forward
+                insert_index += 1
+        
+        # The final value of insert_index represents the length of the modified array
+        return insert_index
